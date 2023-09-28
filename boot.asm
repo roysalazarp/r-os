3cf344ac76fe699ee734a6bf0a93821c38f1a832
Roy Salazar <roy@livelaboratorio.fi>
2023-09-28 22:38:06 +0300

    first commit

---
+++ boot.asm
@@ -0,0 +1,46 @@
+ORG 0
+BITS 16
+
+_start:
+  jmp short start
+  nop
+
+times 33 db 0
+
+start:
+  jmp 0x7c0:step2
+
+step2:
+  cli                 ; Clear interrupts
+  mov ax, 0x7c0
+  mov ds, ax          ; Setup Data Segment 
+  mov es, ax          ; Setup Extra Segment 
+  mov ax, 0x00        
+  mov ss, ax          ; Setup Stack Segment 
+  mov sp, 0x7c00      ; Setup Stack Segment 
+  sti                 ; Enable interrupts
+
+  mov si, message
+  call print
+  jmp $
+
+print:
+  mov bx, 0
+.loop:
+  lodsb
+  cmp al, 0
+  je .done
+  call print_char
+  jmp .loop
+.done:
+  ret
+
+print_char:
+  mov ah, 0eh
+  int 0x10
+  ret
+
+message: db 'Hello world', 0
+
+times 510-($ - $$) db 0
+dw 0xAA55 
